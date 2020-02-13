4c9908a8250e11a2290149b7918369f52edf65bd
Mikael Tallbo <mikael.tallbo@gmail.com>
2020-02-13 14:12:11 +0100

    Add Room/Controller

---
+++ Skolpatrullen/WebApp/ViewModels/RoomViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Database.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.ViewModels
+{
+    public class RoomViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        public string SchoolName{ get; set; }
+    }
+}
